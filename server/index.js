8ba7e4961a5f85e2b5c00180fd7472aa73189eca
Ahmed Alamin <ahmedalamin957@gmail.com>
2024-11-09 03:33:47 -0500

    gave cors access to my frontend

--- server/index.js
+++ server/index.js
@@ -12,7 +12,10 @@ connection();
 
 // middlewares
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: 'https://savenwatch.netlify.app',
+    credentials: true
+}));
 
 // routes
 app.use("/api/users", userRoutes);
@@ -45,5 +48,5 @@ app.post('/addWatchlist', async (req, res) => {
 });
 
 // starting server
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 app.listen(port, console.log(`Listening on port ${port}...`));
