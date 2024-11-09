85c09e0718267fe2b56c5db1921866d45fcca3e8
Ahmed Alamin <ahmedalamin957@gmail.com>
2024-11-09 02:48:25 -0500

    updated port to env

--- server/index.js
+++ server/index.js
@@ -45,5 +45,5 @@ app.post('/addWatchlist', async (req, res) => {
 });
 
 // starting server
-const port = process.env.PORT || 8080;
+const port = process.env.PORT;
 app.listen(port, console.log(`Listening on port ${port}...`));
